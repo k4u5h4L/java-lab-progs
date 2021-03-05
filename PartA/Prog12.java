6c59cd77360728055c28b4630d3ab5ae4fb2c31d
Kaushal <kaushal.v.bhat@gmail.com>
2021-03-05 14:34:33 +0530

    Shorten happy number prog

--- PartA/Prog12.java
+++ PartA/Prog12.java
@@ -1,77 +1,39 @@
-
 import java.util.Scanner;
-
 /*
  * 	Design a java code which accepts a number (which is non-zero and positive)
 	from the user and then checks if it is a happy number or not. Implement it using
 	nested interface concept
  */
- 
 interface GetHappyNumber {
     interface FindIfHappyOrNot {
         void findSum();
-        boolean checkIfHappy();
     }
 }
- 
-public class Prog12 implements GetHappyNumber.FindIfHappyOrNot {
-    int[] digits = new int[20];
-    int sum = 0;
- 
-    void getDigits(int n) {
-        int m, a, i = 1, counter = 0;
-        m = n;
-//        while(n > 0) {
-//            n = n / 10;
-//        }
-        while(m > 0) {
-            a = m % 10;
-            digits[counter++] = a;
-            m = m / 10;
-        }
-    }
- 
-    public boolean checkIfHappy() {
-        if (sum == 1) {
-            return true;
-        } else {
-            return false;
-        }
-    }
- 
-    public void findSum() {
-        int i; 
-        sum = 0;
-        for (i = 0; i < digits.length; i++) {
-            sum += (digits[i] * digits[i]);
-            digits[i] = 0;
-        }
-    }
+public class Testing implements GetHappyNumber.FindIfHappyOrNot {
+	int sum;
+	int n;
+	Testing(int num) {
+		sum = 0;
+		n = num;
+	}
+	public void findSum() {
+		int a;
+		while (n > 0) {
+			a = (n % 10);
+			sum += a * a;
+			n /= 10;
+		}
+		n = sum;
+		sum = 0;
+	}
     public static void main(String args[]) {
-        Scanner s = new Scanner(System.in);
- 
-        System.out.println("Enter a number:");
- 
-        int num = s.nextInt();
- 
-        if (num < 0) {
-            System.out.println("Please enter a number greater than 1");
-            System.exit(0);
-        }
- 
-        int i;
- 
-        Prog12 obj = new Prog12();
- 
-        obj.getDigits(num);
-        obj.findSum();
- 
-        while (!obj.checkIfHappy() && obj.sum >= 10) {
-            obj.getDigits(obj.sum);
-            obj.findSum();
-        }
- 
-        // System.out.println("Sum: " + obj.sum);
-        System.out.println("Happy? : " + obj.checkIfHappy());
+    	Scanner s = new Scanner(System.in);    	
+    	System.out.println("Enter number");
+    	int n = s.nextInt();
+    	Testing obj = new Testing(n);
+    	while (obj.n != 1 && obj.n >= 10) {
+    		obj.findSum();    		
+    	}
+    	System.out.println("Happy? : " + (obj.n == 1 ? "Yes" : "No"));
     }
 }
